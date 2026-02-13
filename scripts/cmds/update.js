0fdd9618525cc4e35ed2cfef0d23cf00b029c1e5
Mah M UD <mahmudx077@gmail.com>
2026-02-13 18:31:22 +0600

    Update update.js

--- scripts/cmds/update.js
+++ scripts/cmds/update.js
@@ -6,41 +6,25 @@ const dirBootLogTemp = `${__dirname}/tmp/rebootUpdated.txt`;
 module.exports = {
 	config: {
 		name: "update",
-		version: "1.5",
-		author: "Chat GPT, NTKhang",
+		version: "1.7",
+		author: "MahMUD",
 		role: 2,
 		description: {
-			en: "Check for and install updates for the chatbot.",
-			vi: "Kiểm tra và cài đặt phiên bản mới nhất của chatbot trên GitHub."
+			en: "Check for and install updates for the chatbot."
 		},
 		category: "owner",
 		guide: {
-			en: "   {pn}",
-			vi: "   {pn}"
+			en: "   {pn}"
 		}
 	},
 
 	langs: {
-		vi: {
-			noUpdates: "✅ | Bạn đang sử dụng phiên bản mới nhất của GoatBot V2 (v%1).",
-			updatePrompt: "💫 | Bạn đang sử dụng phiên bản %1. Hiện tại đã có phiên bản %2. Bạn có muốn cập nhật chatbot lên phiên bản mới nhất không?"
-				+ "\n\n⬆️ | Các tệp sau sẽ được cập nhật:"
-				+ "\n%3%4"
-				+ "\n\nℹ️ | Xem chi tiết tại https://github.com/ntkhang03/Goat-Bot-V2/commits/main"
-				+ "\n💡 | Thả cảm xúc bất kỳ vào tin nhắn này để xác nhận",
-			fileWillDelete: "\n🗑️ | Các tệp/thư mục sau sẽ bị xóa:\n%1",
-			andMore: " ...và %1 tệp khác",
-			updateConfirmed: "🚀 | Đã xác nhận, đang cập nhật...",
-			updateComplete: "✅ | Cập nhật thành công, bạn có muốn khởi động lại chatbot ngay bây giờ không (phản hồi tin nhắn với nội dung \"yes\" hoặc \"y\" để xác nhận).",
-			updateTooFast: "⭕ Vì bản cập nhật gần nhất được thực phát hành cách đây %1 phút %2 giây nên không thể cập nhật. Vui lòng thử lại sau %3 phút %4 giây nữa để cập nhật không bị lỗi.",
-			botWillRestart: "🔄 | Bot sẽ khởi động lại ngay!"
-		},
 		en: {
-			noUpdates: "✅ | You are using the latest version of GoatBot V2 (v%1).",
+			noUpdates: "✅ | You are using the latest version of HinataBot V3 (v%1).",
 			updatePrompt: "💫 | You are using version %1. There is a new version %2. Do you want to update the chatbot to the latest version?"
 				+ "\n\n⬆️ | The following files will be updated:"
 				+ "\n%3%4"
-				+ "\n\nℹ️ | See details at https://github.com/ntkhang03/Goat-Bot-V2/commits/main"
+				+ "\n\nℹ️ | See details at https://github.com/mahmudx7/Hinata-Bot-V3/commits/main"
 				+ "\n💡 | React to this message to confirm.",
 			fileWillDelete: "\n🗑️ | The following files/folders will be deleted:\n%1",
 			andMore: " ...and %1 more files",
@@ -61,8 +45,8 @@ module.exports = {
 
 	onStart: async function ({ message, getLang, commandName, event }) {
 		// Check for updates
-		const { data: { version } } = await axios.get("https://raw.githubusercontent.com/ntkhang03/Goat-Bot-V2/main/package.json");
-		const { data: versions } = await axios.get("https://raw.githubusercontent.com/ntkhang03/Goat-Bot-V2/main/versions.json");
+		const { data: { version } } = await axios.get("https://raw.githubusercontent.com/mahmudx7/Hinata-Bot-V3/main/package.json");
+		const { data: versions } = await axios.get("https://raw.githubusercontent.com/mahmudx7/Hinata-Bot-V3/main/versions.json");
 
 		const currentVersion = require("../../package.json").version;
 		if (compareVersion(version, currentVersion) < 1)
@@ -115,7 +99,7 @@ module.exports = {
 		if (userID != Reaction.authorID)
 			return;
 
-		const { data: lastCommit } = await axios.get('https://api.github.com/repos/ntkhang03/Goat-Bot-V2/commits/main');
+		const { data: lastCommit } = await axios.get('https://api.github.com/repos/mahmudx7/Hinata-Bot-V3/commits/main');
 		const lastCommitDate = new Date(lastCommit.commit.committer.date);
 		// if < 5min then stop update and show message
 		if (new Date().getTime() - lastCommitDate.getTime() < 5 * 60 * 1000) {
